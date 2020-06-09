f299cc05b6b294d638ec5c6ec7189975a7944e4a
Matti Ullah Aamir <matti.ullah.aamir@outlook.com>
2020-06-10 01:51:18 +0500

    CRUD operation to be continued.

--- src/modules/users/controllers/user/user.controller.ts
+++ src/modules/users/controllers/user/user.controller.ts
@@ -5,6 +5,8 @@ import {
   Post,
   Body,
   Get,
+  Param,
+  Put,
 } from '@nestjs/common';
 import { UserService } from '../../services/user/user.service';
 import { IUsers } from '../../interfaces/users.interface';
@@ -22,8 +24,36 @@ export class UserController {
     return result;
   }
 
+  // get all user data
+  @Get('alluser')
+  public async getAllUser(): Promise<any> {
+    return await this.userService.getAllUser();
+  }
+
+  // get all user data
+  @Get(':id')
+  public async getSingleUser(@Param('id') userId: string): Promise<any> {
+    return await this.userService.getSingleUser(userId);
+  }
+
+  // update user
+  @Put(':id')
+  public async updateUser(
+    @Param('id') userid: string,
+    @Body() data: Partial<IUsers>,
+  ): Promise<any> {
+    return this.userService.updateUser(userid, data);
+  }
+
+  // for checking purpose
   @Get('check')
   public getName(): string {
-    return 'Aamir Qureshi';
+    const name: any = this.userService.getMessage();
+
+    if (name.success) {
+      return name;
+    } else {
+      throw new HttpException(name.message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
--- src/modules/users/services/user/user.service.ts
+++ src/modules/users/services/user/user.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, NotFoundException } from '@nestjs/common';
 //import { Users } from '';
 import * as jwt from 'jsonwebtoken';
 //import { jwtConfig } from './../../config/jwtConfig';
@@ -7,6 +7,7 @@ import crypto = require('crypto');
 import { Users } from '../../users.entity';
 import { jwtConfig } from 'src/config/jwtConfig';
 import { AccountService } from 'src/modules/accounts/services/account/account.service';
+import { IUsers } from '../../interfaces/users.interface';
 
 @Injectable()
 export class UserService {
@@ -17,14 +18,17 @@ export class UserService {
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   // eslint-disable-next-line @typescript-eslint/ban-types
-  public async create(user: any): Promise<object> {
-    const exists = await Users.findOne({ where: { Email: user.Email } });
+
+  public async create(user: any): Promise<any> {
+    const exists = await this.usersRepository.findOne({
+      where: { Email: user.Email },
+    });
     if (exists) {
       throw new Error('This email is already used.');
     } else {
       user.Salt = crypto.randomBytes(128).toString('base64');
       user.Password = crypto
-        .createHmac('sha', user.Password + user.Salt)
+        .createHmac('sha256', user.Password + user.Salt)
         .digest('hex');
       const newUser: any = await this.usersRepository.create<Users>(user);
       const jwtToken = jwt.sign(user, process.env.JWT_KEY, jwtConfig);
@@ -34,5 +38,53 @@ export class UserService {
       }
       return newUser;
     }
+  }
+
+  // get all user
+  public async getAllUser(): Promise<any> {
+    const data = await this.usersRepository.findAll();
+    return [...data];
+  }
+
+  // get single user
+  public async getSingleUser(userId: string): Promise<any> {
+    const data = await this.usersRepository.findOne({
+      where: {
+        id: userId.toString(),
+      },
+    });
+    if (!data) {
+      throw new NotFoundException('User could not found.');
+    }
+    return { data };
+  }
+
+  // update single user
+  public async updateUser(userId: string, data: Partial<IUsers>): Promise<any> {
+    await this.usersRepository.update(
+      { where: { id: userId.toString() } },
+      data,
+    );
+    return await this.usersRepository.findOne({
+      where: { id: userId.toString() },
+    });
+  }
+
+  // find user method
+  // private async findUser(userId: string): Promise<any> {
+  //   const user = await this.usersRepository.findOne({
+  //     where: {
+  //       id: userId.toString(),
+  //     },
+  //   });
+  //   if (!user) {
+  //     throw new NotFoundException('User could not found.');
+  //   }
+  //   return { user };
+  // }
+
+  // method for checking
+  public getMessage() {
+    return 'Matti Ullah Aamir Qureshi Farooqi';
   }
 }
